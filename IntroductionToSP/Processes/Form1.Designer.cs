6ee2a4a04189942377bbae786471fa4d18cb5ea5
kaicosades <74011924+kaicosades@users.noreply.github.com>
2024-02-26 20:56:40 +0700

    Add files via upload

---
+++ IntroductionToSP/Processes/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Processes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxProcessName = new System.Windows.Forms.RichTextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelProcessInfo = new System.Windows.Forms.Label();
+            this.myProcess = new System.Diagnostics.Process();
+            this.lvProcesses = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // richTextBoxProcessName
+            // 
+            this.richTextBoxProcessName.Location = new System.Drawing.Point(13, 13);
+            this.richTextBoxProcessName.Name = "richTextBoxProcessName";
+            this.richTextBoxProcessName.Size = new System.Drawing.Size(432, 25);
+            this.richTextBoxProcessName.TabIndex = 0;
+            this.richTextBoxProcessName.Text = "calc.exe";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(13, 45);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(370, 44);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelProcessInfo
+            // 
+            this.labelProcessInfo.AutoSize = true;
+            this.labelProcessInfo.Location = new System.Drawing.Point(13, 75);
+            this.labelProcessInfo.Name = "labelProcessInfo";
+            this.labelProcessInfo.Size = new System.Drawing.Size(63, 13);
+            this.labelProcessInfo.TabIndex = 3;
+            this.labelProcessInfo.Text = "ProcessInfo";
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
+            // 
+            // lvProcesses
+            // 
+            this.lvProcesses.FullRowSelect = true;
+            this.lvProcesses.HideSelection = false;
+            this.lvProcesses.Location = new System.Drawing.Point(16, 91);
+            this.lvProcesses.Name = "lvProcesses";
+            this.lvProcesses.Size = new System.Drawing.Size(429, 232);
+            this.lvProcesses.TabIndex = 4;
+            this.lvProcesses.UseCompatibleStateImageBehavior = false;
+            this.lvProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 335);
+            this.Controls.Add(this.lvProcesses);
+            this.Controls.Add(this.labelProcessInfo);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.richTextBoxProcessName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxProcessName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelProcessInfo;
+        private System.Diagnostics.Process myProcess;
+        private System.Windows.Forms.ListView lvProcesses;
+    }
+}
+
---
+++ IntroductionToSP/Processes/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace Processes
+{
+    public partial class Form1 : Form
+    {
+        List<Process> process_list;
+        public Form1()
+        {
+            InitializeComponent();
+            process_list = new List<Process>();
+            // AllignText();
+            richTextBoxProcessName.Text = "notepad"; // чтобы нотепад высвечивался при запуске
+            //InitProcess();
+            lvProcesses.Columns.Add("PID"); // создание колонок
+            lvProcesses.Columns.Add("Name");
+        }
+
+        //void Form1_Closing(object sender, CancelEventArgs e) //~Form1() // диструктор в C# не работает, т к заркывает в виртуалке. Нужно событие закрытия окна
+        //{
+        //    myProcess.CloseMainWindow();
+        //    myProcess.Close();
+        //}
+
+        void InitProcess()
+        {
+            AllignText();
+            //myProcess = new System.Diaognostic.Process(richTextBoxProcessName.Text);
+            myProcess = new Process();//добавление еще одного объекта, чтобы запоминао 2 процесса
+            myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(richTextBoxProcessName.Text);
+            myProcess.Start();
+            //process_list.Add(myProcess);
+            //lvProcesses.Items.Add(myProcess.ToString());
+            lvProcesses.Items.Add(myProcess.Id.ToString());
+            lvProcesses.Items[lvProcesses.Items.Count - 1].SubItems.Add(myProcess.ProcessName);
+        }
+
+        void AllignText()
+        {
+            richTextBoxProcessName.SelectAll();
+            richTextBoxProcessName.SelectionAlignment = HorizontalAlignment.Center; // имя процесса отображается по середине
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            InitProcess();
+            //myProcess.Start();
+            //Info();
+            //this.TopMost = true; (должно стоять перед Info();, если нет этого, то добавить)
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //if (process_list.Count > 0)
+            if (lvProcesses.Items.Count > 0)
+            {
+                try
+                {
+                    //myProcess = process_list.Last();//First?
+                    myProcess = Process.GetProcessById(Convert.ToInt32(lvProcesses.Items[lvProcesses.Items.Count - 1].Text));//text в конце пишется, чтобы выбирался только ID
+                    myProcess.CloseMainWindow();
+                    myProcess.Close(); //освобождает ресурсы, занимаемые процессом
+                    //process_list.RemoveAt(process_list.Count - 1);
+                    //lvProcesses.Items[lvProcesses.Items.Count - 1];
+                    //lvProcesses.Items.RemoveByKey(myProcess.Id.ToString());
+                    lvProcesses.Items.RemoveAt(lvProcesses.Items.Count - 1);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //process_list.RemoveAt(process_list.Count - 1);
+                    //lvProcesses.Items.RemoveByKey(myProcess.Id.ToString());
+                    lvProcesses.Items.RemoveAt(lvProcesses.Items.Count - 1);
+                }
+                Info();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            while (process_list.Count > 0)
+            {
+                try
+                {
+                    process_list.First().CloseMainWindow();
+                    process_list.First().Close();
+                    process_list.RemoveAt(0);
+                }
+                catch (Exception ex)
+                {
+                    process_list.RemoveAt(0);
+                }
+            }
+            
+            //myProcess.CloseMainWindow();
+            //myProcess.Close();
+        }
+
+        void Info()
+        {
+            if (process_list.Count > 0)
+            {
+                myProcess = process_list.First();
+                labelProcessInfo.Text = $"Total process count: {process_list.Count}\n";
+                labelProcessInfo.Text = "Current process:\n";
+                labelProcessInfo.Text = "Process info:\n";
+                labelProcessInfo.Text += $"PID:              {myProcess.Id}\n";
+                labelProcessInfo.Text += $"Base Priority:    {myProcess.BasePriority}\n";
+                labelProcessInfo.Text += $"Start time:       {myProcess.StartTime}\n";
+                labelProcessInfo.Text += $"Total CPU time:   {myProcess.TotalProcessorTime}\n";
+                labelProcessInfo.Text += $"User CPU time:    {myProcess.UserProcessorTime}\n";
+                labelProcessInfo.Text += $"Session ID:       {myProcess.SessionId}\n";
+                labelProcessInfo.Text += $"Priority Class:   {myProcess.PriorityClass}\n";
+                labelProcessInfo.Text += $"Name:             {myProcess.ProcessName}\n";
+                labelProcessInfo.Text += $"Affinity:         {myProcess.ProcessorAffinity}\n";
+                labelProcessInfo.Text += $"Threads:          {myProcess.Threads.Count}\n";
+            }
+            else labelProcessInfo.Text = "Нет запущенных процессов";
+            
+        }
+
+       
+    }
+}
